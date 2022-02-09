bd552a1ced5500a1142d325e38f4fa5107f58f38
Luis Antonio Obis Aparicio <lobis@unizar.es>
2022-02-09 13:54:30 +0100

    added debug messages to macro

--- GenerateReadouts.C
+++ GenerateReadouts.C
@@ -1,22 +1,26 @@
 
 Int_t GenerateReadouts(TString outFileName) {
+    cout << "Starting macro" << endl;
+
+    cout << "Creating TFile" << endl;
     TFile* f = new TFile(outFileName, "RECREATE");
 
-    // If TRestDetectorReadout name is not given, the first
-    // definition will be taken
+    // If TRestDetectorReadout name is not given, the first definition will be taken
+    cout << "Creating first TRestDetectorReadout" << endl;
     TRestDetectorReadout* r1 = new TRestDetectorReadout("pixelReadout.rml");
     r1->Write("pixel");
 
-    // We need to provide the name because it is not the
-    // first definition
+    // We need to provide the name because it is not the first definition
+    cout << "Creating second TRestDetectorReadout" << endl;
     TRestDetectorReadout* r2 = new TRestDetectorReadout("pixelReadout.rml", "pixelDecoding");
     r2->Write("pixelDecoding");
 
+    cout << "Creating third TRestDetectorReadout" << endl;
     TRestDetectorReadout* r3 = new TRestDetectorReadout("strippedReadout.rml");
     r3->Write("stripped");
 
+    cout << "Closing TFile" << endl;
     f->Close();
 
     return 0;
 }
-
